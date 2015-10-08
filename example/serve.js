c19563f092d12a50d0c7d03653a0bc6ad2911459
Thammi <thammi@chaossource.net>
2015-10-08 19:36:50 +0200

    Improved example server

--- example/serve.js
+++ example/serve.js
@@ -2,18 +2,29 @@ var express = require('express');
 var es = require('easy-signaling');
 var path = require('path');
 
-
-var room = new es.Room();
-
 app = express();
 
+// server all files in the project directory
+
 app.use(express.static(__dirname + '/..'));
+
+// redirect from root to test page
+
+app.get('/', function(req, res) {
+    res.redirect('/example/test.html');
+});
+
+// integrate signaling server
+
+var room = new es.Room();
 
 require('express-ws')(app);
 app.ws('/signaling', function(ws) {
     var channel = new es.WebsocketChannel(ws);
     room.create_guest(channel);
 });
+
+// start server
 
 var server = app.listen(8080, function() {
     var host = server.address().address;
